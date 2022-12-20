ffef8f02c65f2bf66bb2f722129fd64b20817fd4
igmtink <igmtse@gmail.com>
2022-12-20 17:07:03 +0800

    Added (Add Product) Modal.

--- src/components/AvailableFoods/FoodItem.jsx
+++ src/components/AvailableFoods/FoodItem.jsx
@@ -14,7 +14,7 @@ const FoodItem = props => {
   const price = props.price.toFixed(2)
 
   return (
-    <li className="grid grid-cols-1 gap-6 border-b-neutral-700 border-b-2 last:border-b-0 py-4 first:pt-0">
+    <li className="grid grid-cols-1 gap-6 border-b-neutral-700 border-b-2 last:border-b-0 py-4 first:pt-0 animate-shake">
       <div className="grid grid-cols-1 gap-2">
         <h2 className="font-bold text-lg">{props.name}</h2>
         <h3 className="line-clamp-3 text-white/70">{props.description}</h3>
