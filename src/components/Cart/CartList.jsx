e5cfb0aa85b117d3d271e501d227221f84f7402f
igmtink <igmtse@gmail.com>
2022-12-22 11:39:31 +0800

    Added Checkout.

--- src/components/Cart/CartList.jsx
+++ src/components/Cart/CartList.jsx
@@ -1,9 +1,20 @@
 import { Button, Modal } from '../UI/IgmtInk'
 import CartContext from '../../store/Cart/cart-context'
-import { useContext } from 'react'
+import { useContext, useState } from 'react'
 import CartItem from './CartItem'
+import Checkout from './Checkout'
 
 const CartList = props => {
+  const [isCheckout, setIsCheckout] = useState(false)
+
+  const orderHandler = () => {
+    setIsCheckout(true)
+  }
+
+  const orderCancelHandler = () => {
+    setIsCheckout(false)
+  }
+
   const cartCtx = useContext(CartContext)
 
   const onAddHandler = item => {
@@ -38,26 +49,45 @@ const CartList = props => {
     content = <ul className="pt-6 h-full overflow-auto">{cartList}</ul>
   }
 
-  return (
-    <Modal className="bg-neutral-800 p-4 flex flex-col animate-slide-down h-[567px]">
-      <h1 className="text-yellow-500 font-bold text-2xl">Foods Cart</h1>
-      {content}
-      <div className="grid grid-cols-1 gap-2 justify-items-end">
-        <span className="font-bold">Total Amount: ₱{totalAmount}</span>
-        <div className="flex gap-2">
-          <Button
-            className="bg-red-500 hover:bg-red-500/75 text-sm py-2 px-4 rounded-md font-medium"
-            attr={{ onClick: props.onCartHide }}
-          >
-            CLOSE
-          </Button>
-          {cartList.length > 0 && (
-            <Button className="bg-neutral-900 hover:bg-neutral-900/75 text-sm py-2 px-4 rounded-md font-medium">
-              ORDER
+  let section
+
+  if (isCheckout) {
+    section = (
+      <section className="h-full">
+        <Checkout onBack={orderCancelHandler} onCloseCart={props.onCartHide} />
+      </section>
+    )
+  } else {
+    section = (
+      <section className="flex flex-col h-full">
+        <h1 className="text-yellow-500 font-bold text-2xl">Foods Cart</h1>
+        {content}
+        <div className="grid grid-cols-1 gap-2 justify-items-end">
+          <span className="font-bold">Total Amount: ₱{totalAmount}</span>
+          <div className="flex gap-2">
+            <Button
+              className="bg-red-500 hover:bg-red-500/75 text-sm py-2 px-4 rounded-md font-medium"
+              attr={{ onClick: props.onCartHide }}
+            >
+              CLOSE
             </Button>
-          )}
+            {cartList.length > 0 && (
+              <Button
+                className="bg-neutral-900 hover:bg-neutral-900/75 text-sm py-2 px-4 rounded-md font-medium"
+                attr={{ onClick: orderHandler }}
+              >
+                ORDER
+              </Button>
+            )}
+          </div>
         </div>
-      </div>
+      </section>
+    )
+  }
+
+  return (
+    <Modal className="bg-neutral-800 p-4 animate-slide-down h-[567px]">
+      {section}
     </Modal>
   )
 }
---
+++ src/components/Cart/Checkout.jsx
@@ -0,0 +1,155 @@
+import { Button, Input } from '../UI/IgmtInk'
+import { useInput } from '../../hooks/hooks-igmtink'
+import CartContext from '../../store/Cart/cart-context'
+import { useContext } from 'react'
+
+const Checkout = props => {
+  const cartCtx = useContext(CartContext)
+
+  const {
+    value: nameValue,
+    isValid: nameIsValid,
+    hasError: nameHasError,
+    valueChangeHandler: nameChangeHandler,
+    inputBlurHandler: nameBlurHandler,
+    reset: nameReset
+  } = useInput('', value => value.trim() !== '')
+
+  const {
+    value: addressValue,
+    isValid: addressIsValid,
+    hasError: addressHasError,
+    valueChangeHandler: addressChangeHandler,
+    inputBlurHandler: addressBlurHandler,
+    reset: addressReset
+  } = useInput('', value => value.trim() !== '')
+
+  const {
+    value: postalValue,
+    isValid: postalIsValid,
+    hasError: postalHasError,
+    valueChangeHandler: postalChangeHandler,
+    inputBlurHandler: postalBlurHandler,
+    reset: postalReset
+  } = useInput('', value => value.trim() !== '')
+
+  const {
+    value: countryValue,
+    isValid: countryIsValid,
+    hasError: countryHasError,
+    valueChangeHandler: countryChangeHandler,
+    inputBlurHandler: countryBlurHandler,
+    reset: countryReset
+  } = useInput('', value => value.trim() !== '')
+
+  const submitHandler = e => {
+    e.preventDefault()
+
+    if (!nameIsValid || !addressIsValid || !countryIsValid || !postalIsValid) {
+      nameBlurHandler()
+      addressBlurHandler()
+      countryBlurHandler()
+      postalBlurHandler()
+      return
+    }
+
+    nameReset()
+    addressReset()
+    countryReset()
+    postalReset()
+
+    props.onCloseCart()
+  }
+
+  return (
+    <div className="flex flex-col gap-8 h-full">
+      <div className="grid grid-cols-1 gap-4">
+        <Button attr={{ onClick: props.onBack }}>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth={1.5}
+            stroke="currentColor"
+            className="w-4 h-4 transition-colors hover:stroke-white/75"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M10.5 19.5L3 12m0 0l7.5-7.5M3 12h18"
+            />
+          </svg>
+        </Button>
+        <div>
+          <h2 className="text-yellow-500 text-2xl font-bold">
+            Payment details
+          </h2>
+          <h3 className="text-white/75">
+            Enter your payment details below to purchase.
+          </h3>
+        </div>
+      </div>
+      <form onSubmit={submitHandler} className="flex flex-col gap-4">
+        <Input
+          className={`bg-neutral-900 rounded-md p-2 ${
+            nameHasError &&
+            'bg-red-500 outline-red-900 outline-2 outline-none placeholder-black'
+          }`}
+          attr={{
+            placeholder: 'Name',
+            value: nameValue,
+            onChange: nameChangeHandler,
+            onBlur: nameBlurHandler
+          }}
+        />
+        <Input
+          className={`bg-neutral-900 rounded-md p-2 ${
+            addressHasError &&
+            'bg-red-500 outline-red-900 outline-2 outline-none placeholder-black'
+          }`}
+          attr={{
+            placeholder: 'Billing Address',
+            value: addressValue,
+            onChange: addressChangeHandler,
+            onBlur: addressBlurHandler
+          }}
+        />
+        <div className="flex justify-between gap-4">
+          <Input
+            className={`bg-neutral-900 rounded-md p-2 ${
+              countryHasError &&
+              'bg-red-500 outline-red-900 outline-2 outline-none placeholder-black'
+            }`}
+            attr={{
+              placeholder: 'Country',
+              value: countryValue,
+              onChange: countryChangeHandler,
+              onBlur: countryBlurHandler
+            }}
+          />
+          <Input
+            className={`bg-neutral-900 rounded-md p-2 ${
+              postalHasError &&
+              'bg-red-500 outline-red-900 outline-2 outline-none placeholder-black'
+            }`}
+            attr={{
+              type: 'number',
+              placeholder: 'Postal Code',
+              value: postalValue,
+              onChange: postalChangeHandler,
+              onBlur: postalBlurHandler
+            }}
+          />
+        </div>
+        <Button
+          className="bg-yellow-500 hover:bg-yellow-500/75 text-sm py-3 px-4 rounded-md font-medium text-neutral-900"
+          attr={{ type: 'submit' }}
+        >
+          Complete payment &mdash; ₱{cartCtx.totalAmount}
+        </Button>
+      </form>
+    </div>
+  )
+}
+
+export default Checkout
